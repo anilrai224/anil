d009175198686186bb5617a73f7322ad5d3d2296
Anil Rai <anilchamingrai46@gmail.com>
2022-11-23 20:27:58 +0545

    footer-update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import './header.css'
+// import {FaBars} from 'react-icons/fa'
+// import {RiCloseFill} from 'react-icons/ri'
 
 const Header = () => {
+
   return (
     <header id='header-top'>
         <nav className='navigation space-between'>
-            {/* <div className="logo">LOGO</div> */}
-            <ul className='flex'>
-                <li><a href="#/nn">Home</a></li>
-                <li><a href="#aboutMe__container">About</a></li>
-                <li><a href="#main-container">Skills</a></li>
-                <li><a href="#contact_container">Contact</a></li>
-                <li><a href="#/">Projects</a></li>
-            </ul>
+        {/* <FaBars className='openBar'/> */}
+            <div className="navbar">
+              <ul className='flex'>
+                  <li><a href="#/nn">Home</a></li>
+                  <li><a href="#aboutMe__container">About</a></li>
+                  <li><a href="#main-container">Skills</a></li>
+                  <li><a href="#contact_container">Contact</a></li>
+                  <li><a href="#/">Projects</a></li>
+              </ul>
+            </div>
+            {/* <RiCloseFill className='closeBar'/> */}
         </nav>
     </header>
   )
